33814161574f6cdf34652dc76a24ff93b152b409
ortamfag <a_zhidov@outlook.com>
2022-03-25 18:10:57 +0300

    ADD: start eng

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,10 @@ $(".position-select").select2({
 $(".position-select").select2({
     width: 'resolve' // need to override the changed default
 });
+
+const popular = document.querySelector('.models__popular')
+const all = document.querySelector('.models__all')
+const check = document.querySelector('.switch-button')
+
+check.addEventListener('click', () => popular.classList.toggle('none'))
+check.addEventListener('click', () => all.classList.toggle('grid'))
